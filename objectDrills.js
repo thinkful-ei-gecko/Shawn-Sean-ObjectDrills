09ee164c7db99df3fd9b3aa9e766c26724069cb3
shawn-collette <shawn.collette@gmail.com>
2019-08-14 11:07:16 -0600

    complete

--- objectDrills.js
+++ objectDrills.js
@@ -23,4 +23,53 @@ console.log(createMyObject());
 
 //Assignment 2
 //Object updater
+function updateObject(obj) {
+  const myObj = {
+    foo: 'foo',
+    bar: 'bar',
+    bizz: 'bizz',
+    bang: 'bang'
+  }
+
+  return { ...obj, ...myObj };
+
+}
+
+console.log(updateObject({ test1: '1', test2: '2', test3: '3' }));
+
+
+//Assignment 3
+//Self-reference
+function personMaker() {
+  var person = {
+    firstName: 'Paul',
+    lastName: 'Jones',
+    fullName() {
+      return (`${this.firstName} ${this.lastName}`);
+    }
+  }
+  return person;
+};
+
+let newPerson = personMaker()
+console.log(newPerson.fullName());
+
+
+//Assignment 4
+//Key deleting
+const sampleObj = {
+  foo: 'foo',
+  bar: 'bar',
+  bizz: 'bizz',
+  bang: 'bang',
+};
+
+function keyDeleter(obj) {
+  delete obj.foo;
+  delete obj.bar;
+  return obj;
+}
+
+console.log(keyDeleter(sampleObj));
+
 

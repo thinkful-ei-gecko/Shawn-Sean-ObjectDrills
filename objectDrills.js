c8576b2772f2e6819f1ae6dbc8c0fb6337f0ccd3
shawn-collette <shawn.collette@gmail.com>
2019-08-14 10:07:11 -0600

    assignment 1

--- objectDrills.js
+++ objectDrills.js
@@ -1,4 +1,26 @@
+/* eslint-disable strict */
+
 /**
  *  Object Drills 8/14/2019
  * 
  */
+
+//Assignment 1
+//Object creator
+function createMyObject() {
+  return {
+    foo: 'bar',
+    answerToUniverse: 42,
+    'olly olly': 'oxen free',
+    sayHello() {
+      return 'hello';
+    }
+  };
+}
+
+console.log(createMyObject());
+
+
+//Assignment 2
+//Object updater
+
